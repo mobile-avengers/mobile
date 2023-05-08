4775a33b41cfc25cba143ea2802274e5ff3accc4
Kirill Neevin <neevin@yandex-team.ru>
2023-05-08 12:56:02 +0300

    removed .idea + added sample of http request to backend

---
+++ app/src/main/java/com/app/mobilepart/Constants.kt
@@ -0,0 +1,8 @@
+package com.app.mobilepart
+
+class Constants {
+    companion object {
+        const val ORDER_SERVICE_API_ENDPOINT = "http://10.0.2.2:8080"
+        const val PING_ORDER_SERVICE = "$ORDER_SERVICE_API_ENDPOINT/ping"
+    }
+}
